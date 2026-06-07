227ffef183fd3e760274a56e31a430792be609d1
ehidanchavez67 <ehidanchavez@unam.edu>
2026-06-07 15:50:51 +0000

    Cambiando parametros de memory managerpunto h, con esto se organiza la sintaxis del codigo

--- include/memory_manager.h
+++ include/memory_manager.h
@@ -2,61 +2,26 @@
 #define MEMORY_MANAGER_H
 
 typedef struct MemoryBlock {
-
     int start;
-
     int size;
-
     int free;
-
     int pid;
-
     struct MemoryBlock* next;
-
     struct MemoryBlock* prev;
-
 } MemoryBlock;
 
 typedef struct MemoryManager {
-
     MemoryBlock* head;
-
     int total_memory;
-
 } MemoryManager;
 
 MemoryManager* mm_create(int total_memory);
+int mm_allocate_first_fit(MemoryManager* mm, int pid, int size);
+int mm_allocate_best_fit(MemoryManager* mm, int pid, int size);
+int mm_allocate_worst_fit(MemoryManager* mm, int pid, int size);
+void mm_free(MemoryManager* mm, int pid);
+void mm_coalesce(MemoryManager* mm);
+void mm_compact(MemoryManager* mm);
+void mm_destroy(MemoryManager* mm);
 
-int mm_allocate_first_fit(
-    MemoryManager* mm,
-    int size
-);
-
-int mm_allocate_best_fit(
-    MemoryManager* mm,
-    int size
-);
-
-int mm_allocate_worst_fit(
-    MemoryManager* mm,
-    int size
-);
-
-void mm_free(
-    MemoryManager* mm,
-    int pid
-);
-
-void mm_coalesce(
-    MemoryManager* mm
-);
-
-void mm_divide_v(
-    MemoryManager* mm
-);
-
-void mm_destroy(
-    MemoryManager* mm
-);
-
-#endif
+#endif 
